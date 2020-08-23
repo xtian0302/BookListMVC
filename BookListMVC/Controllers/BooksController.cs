2196d7b2a7497e9dcf86f6d0f5fd290d10ba896a
Christian Banal <christianjohn.banal@yahoo.com>
2020-08-24 06:08:25 +0800

    Added Book to DB and Books Controller

---
+++ BookListMVC/Controllers/BooksController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using BookListMVC.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace BookListMVC.Controllers
+{
+    [Route("api/Book")]
+    [ApiController]
+    public class BooksController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        private readonly ApplicationDbContext _db;
+        public BooksController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+        [HttpGet]
+        public async Task<IActionResult> GetAll()
+        {
+            return Json(new { data = await _db.Books.ToListAsync() });
+        }
+
+        [HttpDelete]
+        public async Task<IActionResult> Delete(int id)
+        {
+            var bookFromDb = await _db.Books.FirstOrDefaultAsync(u => u.Id == id);
+            if (bookFromDb == null)
+            {
+                return Json(new { success = false, message = "Error while Deleting" });
+            }
+            _db.Books.Remove(bookFromDb);
+            await _db.SaveChangesAsync();
+            return Json(new { success = true, message = "Delete Succesful" });
+        }
+    }
+}
